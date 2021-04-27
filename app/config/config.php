b5ad2510bca5722622522d11eaa338f094c47385
Kalhar Pandya <kalharp@development.local>
2021-04-27 09:16:24 +0530

    First Commit

---
+++ app/config/config.php
@@ -0,0 +1,17 @@
+<?php
+
+//Database Params
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'framework');
+
+
+define('APPROOT', dirname(dirname(__FILE__)));
+
+define('URLROOT', 'localhost:8080/Training/PHP/Framework');
+
+define('SITENAME', 'My Framework');
+
+
---
+++ app/views/pages/index.php
@@ -0,0 +1,6 @@
+<?php 
+    foreach($data['users'] as $user){
+        echo "Information : ".$user->name;
+        echo "<br>";
+    }
+?>
